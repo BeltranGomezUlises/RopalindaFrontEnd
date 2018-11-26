8785240c7514e17cc65be5c0fd1ad8aca5bd3d93
Ricardo Morales <ramoralesr@concredito.com.mx>
2018-11-25 22:03:43 -0700

    menu de categorias por bd

--- app_modules/Layouts/DesktopContainer.jsx
+++ app_modules/Layouts/DesktopContainer.jsx
@@ -4,20 +4,41 @@ import {
   Menu, Responsive, Segment, Visibility, Link, Dropdown
 } from 'semantic-ui-react'
 import Login from '../Access/Login.jsx'
+import * as utils from '../../utils.js'
 
+const rutaCategorias = 'http://74.208.178.83:8080/Ropalinda/api/categories';
+const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIn0.kJdFAfN1eP6-4vEjv0lTRVsmj4L3RAJ60nl3vJFAfoLAK5tSkf-Qh-B8lyerGnA9oFnQIlVrEXj9xrYV6RKzLQ';
 export default class DesktopContainer extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       modalLoginVisible: false,
-      activeItem: 'home'
+      activeItem: 'home',
+      categories: []
     }
     this.hideFixedMenu = this.hideFixedMenu.bind(this);
     this.showFixedMenu = this.showFixedMenu.bind(this);
     this.closeModal = this.closeModal.bind(this);
     this.openModal = this.openModal.bind(this);
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentWillMount() {
+    fetch(rutaCategorias, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': token //localStorage.getItem('tokenSesion')
+      }
+    }).then((res) => res.json())
+      .then((r) => {
+        utils.evalResponse(r, () => {
+          this.setState({ categories: r.data })
+        })
+      });
   }
 
   hideFixedMenu() {
@@ -43,6 +64,27 @@ export default class DesktopContainer extends Component {
     this.setState({ modalLoginVisible: true })
   }
 
+  renderCategoryList() {
+    return this.state.categories.map(i => {
+      return (
+        <Dropdown key={i.id} name={i.name} item simple text={i.name}>
+          <Dropdown.Menu>
+            {this.renderSubCategoryList(i.subcategoryCollection)}
+          </Dropdown.Menu>
+        </Dropdown>
+      )
+    })
+  }
+
+  renderSubCategoryList(subCategories) {
+    return subCategories.map(sub => {
+      return (
+        <Dropdown.Item key={sub.id} name={sub.name} as={Link} onClick={this.handleClick}>{sub.name}</Dropdown.Item>
+      )
+    }
+    )
+  }
+
   render() {
     const { children } = this.props
     const { fixed, activeItem } = this.state
@@ -51,51 +93,34 @@ export default class DesktopContainer extends Component {
       <div>
         <Menu
           fixed={top}
-           style={{ "max-height": "70px", "min-height": "70px" }}>
+          style={{ "max-height": "70px", "min-height": "70px" }}>
           <Container>
             <Image src='assets/logo.png' bordered size='small' style={{ padding: '10px' }} />
-            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleClick}>
-              Inicio
+            <Menu>
+              <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleClick}>
+                Inicio
                 </Menu.Item>
-            <Dropdown item simple text='Hombre'>
-              <Dropdown.Menu>
-                <Dropdown.Item as={Link} to='/home'>Camisas</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Jeans</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Corbatas</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Abrigos</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Jeans</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Gorras</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Dropdown item simple text='Mujer'>
-              <Dropdown.Menu>
-                <Dropdown.Item as={Link} to='/home'>Vestidos</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Tops</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Jeans</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Pantalones</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Faldas</Dropdown.Item>
-                <Dropdown.Item as={Link} to='/home'>Camisas</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Menu.Item name='ofertas' active={activeItem === 'ofertas'} onClick={this.handleClick}>
-              Ofertas
+              {this.renderCategoryList()}
+              <Menu.Item name='ofertas' active={activeItem === 'ofertas'} onClick={this.handleClick}>
+                Ofertas
                </Menu.Item>
-            <Menu.Item position='right'>
-              <Modal
-                trigger={
-                  <Button onClick={this.openModal}>Iniciar sesión</Button>
-                }
-                onClose={this.closeModal}
-                open={this.state.modalLoginVisible}>
-                <Header content='Iniciar Sesión' textAlign='center' />
-                <Modal.Content >
-                  <Login close={this.closeModal} />
-                </Modal.Content>
-              </Modal>
-            </Menu.Item>
+                <Menu.Item position='right'>
+                  <Modal
+                    trigger={
+                      <Button onClick={this.openModal}>Iniciar sesión</Button>
+                    }
+                    onClose={this.closeModal}
+                    open={this.state.modalLoginVisible}>
+                    <Header content='Iniciar Sesión' textAlign='center' />
+                    <Modal.Content >
+                      <Login close={this.closeModal} />
+                    </Modal.Content>
+                  </Modal>
+                </Menu.Item>
+            </Menu>
           </Container>
         </Menu>
-        <div style={{ "max-height": "70px", "min-height": "70px" }}></div>      
+        <div style={{ "max-height": "70px", "min-height": "70px" }}></div>
         {children}
       </div>
     )
