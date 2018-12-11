09358470c782083d2c66ce1b30c376f7f4dd89e4
Ricardo Morales <ramoralesr@concredito.com.mx>
2018-12-11 03:11:10 -0700

    flujo pedido

--- app_modules/Pedido/Pedido.jsx
+++ app_modules/Pedido/Pedido.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {
     Segment, Step, Icon, Card,
-    Button, Modal, Header, Message
+    Button, Modal, Header, Message, Form, Input
 } from 'semantic-ui-react';
 import PaymentCard from 'react-payment-card-component'
 import * as utils from '../../utils.js';
@@ -17,15 +17,15 @@ export default class Pedido extends Component {
             active: 'truck',
             selectedAddress: null,
             selectedMethod: null,
-            openModal: false
+            openModal: false,
             nombre: '',
             numeroTarjeta: '',
             tipoTarjeta: 'visa',
             fecha: '',
             flipped: false,
             cvv: '',
-            limit: 16
-            addresses: [],
+            limit: 16,
+            addresses: []
         }
 
         this.loadAddresses = this.loadAddresses.bind(this);
@@ -133,7 +133,7 @@ export default class Pedido extends Component {
                             </Card.Content>
                             <Card.Content extra>
                                 <Button primary onClick={() => {
-                                    this.setState({ selectedMethod: 'deposit', active: 'confirm' })
+                                    this.setState({ selectedMethod: 'deposit' })
                                 }}>
                                     Seleccionar
                         </Button>
@@ -147,7 +147,7 @@ export default class Pedido extends Component {
                             </Card.Content>
                             <Card.Content extra>
                                 <Button primary onClick={() => {
-                                    this.setState({ selectedMethod: 'card', active: 'confirm' })
+                                    this.setState({ selectedMethod: 'card' })
                                 }}>
                                     Seleccionar
                         </Button>
@@ -159,14 +159,102 @@ export default class Pedido extends Component {
         }
     }
 
+    renderCard() {
+        if (this.state.selectedMethod == 'card') {
+            return (
+                <div>
+                    <Segment style={{ width: '100%', display: 'inline-flex', justifyContent: 'center' }}>
+                        <PaymentCard
+                            bank="default"
+                            brand={this.state.tipoTarjeta}
+                            number={this.state.numeroTarjeta}
+                            cvv={this.state.cvv}
+                            holderName={this.state.nombre}
+                            expiration={this.state.mes + '/' + this.state.anio}
+                            flipped={this.state.flipped}
+                        />
+                        <Form style={{ marginLeft: 20 }}>
+                            <Form.Field required>
+                                <label>Nombre:</label>
+                                <Input
+                                    maxLength='20'
+                                    onChange={(evt) => { this.setState({ nombre: evt.target.value }) }}
+                                    value={this.state.nombre} />
+                            </Form.Field>
+                            <Form.Field required>
+                                <label>Numero de tarjeta:</label>
+                                <Input
+                                    maxLength={this.state.limit}
+                                    onChange={(evt) => {
+                                        this.setState({ numeroTarjeta: evt.target.value })
+                                        this.validateType()
+                                    }}
+                                    value={this.state.numeroTarjeta} />
+                            </Form.Field>
+                            <Form.Group>
+                                <Form.Field required>
+                                    <label>Fecha de expiración:</label>
+                                    <Form.Group>
+                                        <Input
+                                            type='number'
+                                            maxLength='2'
+                                            onChange={(evt) => {
+                                                let valor = evt.target.value;
+                                                if (valor > 0 && valor < 13)
+                                                    this.setState({ mes: valor })
+                                            }}
+                                            value={this.state.mes} />
+                                        <label style={{ margin: 10 }}>/</label>
+                                        <Input
+                                            type='number'
+                                            maxLength='2'
+                                            onChange={(evt) => {
+                                                let valor = evt.target.value;
+                                                if (valor > 17 && valor < 100)
+                                                    this.setState({ anio: valor })
+                                            }}
+                                            value={this.state.anio} />
+                                    </Form.Group>
+
+                                </Form.Field>
+                                <Form.Field required>
+                                    <label>CVV:</label>
+                                    <Input
+                                        maxLength='3'
+                                        onChange={(evt) => {
+                                            this.setState({
+                                                flipped: true,
+                                                cvv: evt.target.value
+                                            })
+                                            this.checkCVV()
+                                        }}
+                                        value={this.state.cvv} />
+                                </Form.Field>
+                            </Form.Group>
+                        </Form>
+                    </Segment>
+                    <div style={{textAlign: 'center'}}>
+                        <Button 
+                            color='green' 
+                            onClick={() => { this.setState({ active: 'confirm', selectedMethod: null }) }}
+                            fluid>
+                            Check
+            </Button>
+                    </div>
+
+                </div>
+            )
+        }
+    }
+
     renderConfirm() {
         if (this.state.active == 'confirm') {
             return (<Segment loading={this.state.loading}>
                 <Carrito mode='pedido' />
                 {this.renderDepositMessage()}
                 <Button primary onClick={() => {
-                    if(this.state.selectedAddress == null){
-                        this.setState({active: 'truck'});                        
+                    if (this.state.selectedAddress == null) {
+                        this.setState({ active: 'truck' });
                     }
                 }}>Confirmar pedido</Button>
             </Segment >)
@@ -215,7 +303,12 @@ export default class Pedido extends Component {
         return (
             <Segment>
                 <Step.Group>
-                    <Step active={active == 'truck'} link onClick={() => { this.setState({ active: 'truck' }) }}>
+                    <Step active={active == 'truck'} link onClick={() => {
+                        this.setState({
+                            active: 'truck',
+                            selectedMethod: null
+                        })
+                    }}>
                         <Icon name='truck' />
                         <Step.Content>
                             <Step.Title>Envío</Step.Title>
@@ -223,7 +316,12 @@ export default class Pedido extends Component {
                         </Step.Content>
                     </Step>
 
-                    <Step active={active == 'payment'} link onClick={() => { this.setState({ active: 'payment' }) }}>
+                    <Step active={active == 'payment'} link onClick={() => {
+                        this.setState({
+                            active: 'payment',
+                            selectedMethod: null
+                        })
+                    }}>
                         <Icon name='payment' />
                         <Step.Content>
                             <Step.Title>Pago</Step.Title>
@@ -240,79 +338,8 @@ export default class Pedido extends Component {
                 </Step.Group>
 
                 {this.renderTruck()}
-                {this.renderPayment()}
+                {this.state.selectedMethod ? this.renderCard() : this.renderPayment()}
                 {this.renderConfirm()}
-                <Segment style={{ width: '100%', display: 'inline-flex', justifyContent: 'center' }}>
-                    <PaymentCard
-                        bank="default"
-                        brand={this.state.tipoTarjeta}
-                        number={this.state.numeroTarjeta}
-                        cvv={this.state.cvv}
-                        holderName={this.state.nombre}
-                        expiration={this.state.mes + '/' + this.state.anio}
-                        flipped={this.state.flipped}
-                    />
-                    <Form style={{ marginLeft: 20 }}>
-                        <Form.Field>
-                            <label>Nombre:</label>
-                            <Input
-                                maxLength='20'
-                                onChange={(evt) => { this.setState({ nombre: evt.target.value }) }}
-                                value={this.state.nombre} />
-                        </Form.Field>
-                        <Form.Field>
-                            <label>Numero de tarjeta:</label>
-                            <Input
-                                maxLength={this.state.limit}
-                                onChange={(evt) => {
-                                    this.setState({ numeroTarjeta: evt.target.value })
-                                    this.validateType()
-                                }}
-                                value={this.state.numeroTarjeta} />
-                        </Form.Field>
-                        <Form.Group>
-                            <Form.Field>
-                                <label>Fecha de expiración:</label>
-                                <Form.Group>
-                                    <Input
-                                    type='number'
-                                    maxLength='2'
-                                    onChange={(evt) => {
-                                        let valor = evt.target.value;
-                                        if(valor>0 && valor < 13)
-                                        this.setState({ mes: valor })
-                                    }}
-                                    value={this.state.mes} />
-                                    <label style={{ margin: 10}}>/</label>
-                                    <Input
-                                    type='number'
-                                    maxLength='2'
-                                    onChange={(evt) => {
-                                        let valor = evt.target.value;
-                                        if(valor>17 && valor < 100)
-                                        this.setState({ anio: valor })
-                                    }}
-                                    value={this.state.anio} />
-                                </Form.Group>
-                                
-                            </Form.Field>
-                            <Form.Field>
-                                <label>CVV:</label>
-                                <Input
-                                    maxLength='3'
-                                    onChange={(evt) => {
-                                        this.setState({
-                                            flipped: true,
-                                            cvv: evt.target.value
-                                        })
-                                        this.checkCVV()
-                                    }}
-                                    value={this.state.cvv} />
-                            </Form.Field>
-                        </Form.Group>
-
-                    </Form>
-                </Segment>
 
             </Segment>
         )
